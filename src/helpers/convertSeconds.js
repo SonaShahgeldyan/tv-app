3078c9266cfbe0ca07ace005652a5ce0c103419a
SonaShahgeldyan <sona.shahgeldyan28@gmail.com>
2023-11-19 14:54:18 +0400

    feat: time convertor

---
+++ src/helpers/convertSeconds.js
@@ -0,0 +1,8 @@
+const ConvertSeconds = (seconds) => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+
+  return `${hours}h ${minutes}m `;
+};
+
+export default ConvertSeconds;
