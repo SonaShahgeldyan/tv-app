5de33c6ccabc15d3581de1cf624467511a81cb4f
SonaShahgeldyan <sona.shahgeldyan28@gmail.com>
2023-11-19 14:54:51 +0400

    feat: homePage component

---
+++ src/pages/Home.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import VideoPlayer from "../components/VideoPlayer";
+import videoData from "../data/data.json";
+
+import FeaturedVideo from "../components/FeaturedVideo";
+import TendingNow from "../components/TendingNow";
+
+const HomePage = () => {
+  const { Featured, TendingNow: Tending } = videoData;
+  const [lastClickedVideo, setLastClickedVideo] = useState(null);
+  const [showVideoPlayer, setShowVideoPlayer] = useState(false);
+
+  const handleVideoClick = (video) => {
+    setShowVideoPlayer(false);
+
+    setLastClickedVideo(video);
+    sessionStorage.setItem("lastClickedVideoId", video.id);
+
+    setTimeout(() => {
+      setShowVideoPlayer(true);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    const lastClickedVideoId = sessionStorage.getItem("lastClickedVideoId");
+    const video = Tending.find((v) => v.Id === lastClickedVideoId);
+    setLastClickedVideo(video);
+  }, [Tending]);
+
+  return (
+    <div>
+      {showVideoPlayer ? (
+        <VideoPlayer videoUrl={lastClickedVideo.VideoUrl} />
+      ) : (
+        <FeaturedVideo video={lastClickedVideo || Featured} />
+      )}
+
+      <TendingNow trendingVideos={Tending} onVideoClick={handleVideoClick} />
+    </div>
+  );
+};
+export default HomePage;
